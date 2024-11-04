f208b9f89628b6ccf16df15a0135eb34a14b1fc1
Eduardo Carvalho Valadão <104527380+eduardovaladao@users.noreply.github.com>
2024-11-04 14:18:05 -0300

    Add files via upload

--- src/java/com/udihealth/controller/MedicoController.java
+++ src/java/com/udihealth/controller/MedicoController.java
@@ -3,6 +3,7 @@ package com.udihealth.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.sql.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,37 +13,80 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.udihealth.dominio.Medico;
+import com.udihealth.dominio.Usuario;
 import com.udihealth.modelo.MedicoModelo;
+import com.udihealth.modelo.UsuarioModelo;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 
 @Controller
 public class MedicoController {
     
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
     @RequestMapping(value = "/consultarMedicos", method = RequestMethod.GET)
     public ModelAndView consultarMedicos() {
         return new ModelAndView("consultarMedicos", "medico", new Medico());
     }
-    
+
     // Controller para processar a seleção do médico
-    @RequestMapping(value = "/consultarMedicos", method = RequestMethod.POST)
-    public String processarMedico(@ModelAttribute("medico") Medico medico) {
-        // Processar a seleção do médico aqui, com base no codigoMedico
-        // Pode redirecionar para outra página ou exibir os dados do médico selecionado
+    @RequestMapping(value = "/consultarMedico", method = RequestMethod.POST)
+    public String consultarMedico(@ModelAttribute("medico") Medico med, Model modelo) {
+        Medico medico = MedicoModelo.buscarMedicoCodigo(med.getCodigoMedico());
+        System.out.println(med.getNome());
+        modelo.addAttribute("nome", medico.getNome());
+        modelo.addAttribute("sexo", medico.getSexo() + ""); // conversão para String
+        modelo.addAttribute("cpf", medico.getCpf());
+        modelo.addAttribute("crm", medico.getCrm());
+        modelo.addAttribute("telefone", medico.getTelefone());
+
         return "resultadoMedico"; // Exemplo de redirecionamento
     }
-    
+
     @RequestMapping(value = "/adicionarMedico", method = RequestMethod.GET)
-    public String adicionarMedico(@RequestParam String param) {
-        return new String();
+    public ModelAndView adicionarMedico() {
+        return new ModelAndView("adicionarMedico", "medico", new Medico());
     }
     
-    @ModelAttribute("webConsultarMedicos") 
-    public Map<Integer, String> webConsultarMedicos() { 
-        ArrayList<Medico> medicos = MedicoModelo.buscarMedicos();
+    @RequestMapping(value = "/adicionarNovoMedico", method = RequestMethod.POST)
+    public String adicionarMedico(@ModelAttribute("medico")@Validated Medico med, BindingResult bindingResult, Model modelo) {
+        // if (bindingResult.hasErrors()) return "adicionarMedico";
         
+        UsuarioModelo.inserirUsuario(med);        
+        
+        modelo.addAttribute("nome", med.getNome());
+        modelo.addAttribute("senha", med.getSenha());
+        modelo.addAttribute("sexo", med.getSexo());
+        modelo.addAttribute("dataNascimento",  med.getDataNascimento());
+        modelo.addAttribute("crm", med.getCrm());
+        modelo.addAttribute("cpf", med.getCpf());
+        modelo.addAttribute("cep", med.getCep());
+        modelo.addAttribute("telefone", med.getTelefone());
+        modelo.addAttribute("email", med.getEmail());
+        
+        
+        
+        return "resultadoNovoMedico";
+    }
+
+    @ModelAttribute("medico")
+    public Medico criarMedicoModelo() {
+        return new Medico();
+    }
+
+    @ModelAttribute("webConsultarMedicos")
+    public Map<Integer, String> webConsultarMedicos() {
+        ArrayList<Medico> medicos = MedicoModelo.buscarMedicos();
+
         Map<Integer, String> consultaMedicos = new HashMap<>();
-        for(int i = 0; i < medicos.size(); i++)
+        for (int i = 0; i < medicos.size(); i++) {
             consultaMedicos.put(medicos.get(i).getCodigoMedico(), medicos.get(i).getNome());
+        }
         return consultaMedicos;
     }
-    
+
 }
